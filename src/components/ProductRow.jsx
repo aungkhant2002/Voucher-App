2350178ff4cb44d3cb88b700c977bfab932ec945
aungkhant2002 <aung.khant@ucsy.edu.mm>
2025-08-08 14:46:36 +0630

    feat(product): implement delete product with API call and loading UI feedback

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { HiOutlinePencil, HiOutlineTrash } from 'react-icons/hi2'
+import { useSWRConfig } from 'swr'
+import { Bouncy } from 'ldrs/react'
+import 'ldrs/react/Bouncy.css'
 
 const ProductRow = ({ product: { id, product_name, price, created_at } }) => {
+
+    const [isDeleting, setIsDeleting] = useState(false);
+    const { mutate } = useSWRConfig();
+
     const date = new Date(created_at);
     const currentDate = date.toLocaleDateString('en-GB', {
         day: "numeric",
@@ -13,6 +20,14 @@ const ProductRow = ({ product: { id, product_name, price, created_at } }) => {
         minute: "2-digit",
         hour12: true
     });
+
+    const handleDeleteBtn = async () => {
+        setIsDeleting(true)
+        await fetch(import.meta.env.VITE_API_URL + `/products/${id}`, {
+            method: "DELETE"
+        })
+        mutate(import.meta.env.VITE_API_URL + "/products")
+    }
 
     return (
         <tr className="odd:bg-white even:bg-stone-50 border-b border-stone-200">
@@ -31,12 +46,16 @@ const ProductRow = ({ product: { id, product_name, price, created_at } }) => {
             </td>
             <td className="px-6 py-4 text-center">
                 <div className="inline-flex rounded-md shadow-xs" role="group">
-                    <div>
-                        <button type="button" className="px-4 py-2 text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-s-lg hover:bg-stone-100 hover:text-stone-700 focus:z-10">
+                    <div className='flex'>
+                        <button type="button" className="size-10 flex justify-center items-center text-sm font-medium text-stone-900 bg-white border border-stone-200 rounded-s-lg hover:bg-stone-100 hover:text-stone-700 focus:z-10">
                             <HiOutlinePencil />
                         </button>
-                        <button type="button" className="px-4 py-2 text-sm font-medium text-red-600 bg-white border border-stone-200 rounded-e-lg hover:bg-stone-100 hover:text-red-700 focus:z-10">
-                            <HiOutlineTrash />
+                        <button onClick={handleDeleteBtn} type="button" className="size-10 flex justify-center items-center text-sm font-medium text-red-600 bg-white border border-stone-200 rounded-e-lg hover:bg-stone-100 hover:text-red-700 focus:z-10">
+                            {isDeleting ? (
+                                <Bouncy size="20" speed="1.75" color="red"/>
+                            ) : (
+                                <HiOutlineTrash/>
+                            )}
                         </button>
                     </div>
                 </div>
